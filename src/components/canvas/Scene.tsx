15e21a1d48927bf1d352c6f567149da1cb4822f6
SweetieBirdX <eyupefekarakoca@gmail.com>
2026-02-01 00:04:11 +0300

    feat(scene): integrate vehicle and remove orbit controls

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -1,25 +1,24 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
 import { Physics, RigidBody } from "@react-three/rapier";
 import Road from "./Road";
+import Vehicle from "./Vehicle";
 
 export default function Scene() {
     return (
-        <Canvas camera={{ position: [0, 5, 10], fov: 50 }}>
+        <Canvas camera={{ position: [0, 5, 20], fov: 50 }}>
             {/* Lights */}
             <ambientLight intensity={0.5} />
             <directionalLight position={[10, 10, 10]} intensity={1} />
-
-            {/* Controls */}
-            <OrbitControls />
 
             {/* Physics World */}
             <Physics debug={true}>
                 {/* Road */}
                 <Road />
+                {/* Vehicle */}
+                <Vehicle />
             </Physics>
-        </Canvas >
+        </Canvas>
     );
 }
